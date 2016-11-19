21932cf7ee3781f40d8b5f532d3a32eed33b05aa
pasjtene <pasjtene@yahoo.co.uk>
2016-11-19 17:50:12 +0000

    Install d3 and hammerJs

--- client/systemjs.config.js
+++ client/systemjs.config.js
@@ -24,9 +24,11 @@
       '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
       '@angular/upgrade': 'npm:@angular/upgrade/bundles/upgrade.umd.js',
       '@angular/upgrade/static': 'npm:@angular/upgrade/bundles/upgrade-static.umd.js',
+      '@angular/material': 'npm:@angular/material/material.umd.js',
       // other libraries
       'rxjs':                      'npm:rxjs',
-      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
+      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
+      'd3':'npm:d3/build/d3.js'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
